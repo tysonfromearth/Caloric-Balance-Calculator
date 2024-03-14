76d6ae65b024a7c6c7135618acb23f155bd756ef
Tyson Bryant <bryantty@oregonstate.edu>
2024-03-14 14:36:37 -0700

    turned input strings into variables

--- milestone1.py
+++ milestone1.py
@@ -8,12 +8,23 @@ socket.connect("tcp://localhost:5555")
 
 # initialize variables
 greeting = "Welcome to Caloric Balance Calculator!"
+input1 = "Enter a number to select an option:\n" \
+        "\n    1. About Caloric Balance Calculator (25 second read)" \
+        "\n    2. Calculate Caloric Balance" \
+        "\n    3. Exit\n"
 about = "Caloric Balance Calculator (CBC) is a text-based program that calculates the difference between\n" \
     "calories consumed and calories burned in one day. Use your keyboard to enter numbers or select\n" \
     "options as prompted by the text. CBC will ask for your total calorie consumption for the day \n" \
     "and your activity level. Maintain a negative balance to effect weight loss and a positive balance\n" \
     "for weight gain."
+input2 = "Enter the total calories consumed today or \"z\" to cancel: "
+input3 = "What is your activity level? Enter a number:\n" \
+                "\n    1. Sedentary" \
+                "\n    2. Light" \
+                "\n    3. Moderate" \
+                "\n    4. Active\n"
 goodbye = "Thanks for using Caloric Balance Calculator, goodbye!"
+
 
 def use_microservice(list_request):
     """Sends user-provided list to microservice then receives and prints the result."""
@@ -39,17 +50,11 @@ def activity_converter(selection_3):
 def get_input():
     """Gathers user's calories consumed and activity level then returns them as a list in that order."""
     while True:
-        selection_2 = input("Enter the total calories consumed today or \"z\" to cancel: ")
+        selection_2 = input(input2)
         if selection_2 == "z":
             return "home screen"  # home_screen() function checks for this to return to initial menu
         elif selection_2.isdigit():
-            selection_3 = input(
-                "What is your activity level? Enter a number:\n"
-                "\n    1. Sedentary"
-                "\n    2. Light"
-                "\n    3. Moderate"
-                "\n    4. Active\n"
-            )
+            selection_3 = input(input3)
             selection_3 = activity_converter(selection_3)  # converts from a number to a corresponding string
             return [selection_2, selection_3]
         else:
@@ -58,12 +63,7 @@ def get_input():
 
 def home_screen():
     """Gathers user input to navigate the application."""
-    selection_1 = input(
-        "Enter a number to select an option:\n"
-        "\n    1. About Caloric Balance Calculator (25 second read)"
-        "\n    2. Calculate Caloric Balance"
-        "\n    3. Exit\n"
-    )
+    selection_1 = input(input1)
     if selection_1 == "1":
         print(about)
     elif selection_1 == "2":
